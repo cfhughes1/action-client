19f3b997b35770195dc87e508938d363e27900a7
Christina Hughes <christinafayehughes2@gmail.com>
2018-09-02 23:02:55 -0400

    basic connection to server using rosbridge

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,11 +31,53 @@
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  mounted() {
+    var ROSLIB = require('roslib');
+    var ros = new ROSLIB.Ros({
+      url : 'ws://localhost:9090'
+    });
+
+    var fibonacciClient = new ROSLIB.ActionClient({
+      ros : ros,
+      serverName : '/fibonacci',
+      actionName : 'actionlib_tutorials/FibonacciAction'
+    });
+
+    var goal = new ROSLIB.Goal({
+      actionClient : fibonacciClient,
+      goalMessage : {
+        order : 10
+      }
+    });
+
+    goal.on('feedback', function(feedback) {
+      console.log('Feedback: ' + feedback.sequence);
+    });
+
+    goal.on('result', function(result) {
+      console.log('Final Result: ' + result.sequence);
+    });
+
+    ros.on('connection', function() {
+      console.log('Connected to websocket server.');
+    });
+
+    ros.on('error', function(error) {
+      console.log('Error connecting to websocket server: ', error);
+    });
+
+    ros.on('close', function() {
+      console.log('Connection to websocket server closed.');
+    });
+
+    goal.send();
+  },
 }
 </script>
 
